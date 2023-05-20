e6f694da5a09b8cd98e8407b970fd02bf351c50a
Mohammad Abbasi <133187637+Mohammad-Abbasi2559@users.noreply.github.com>
2023-05-20 05:26:53 -0700

    Add project files.

---
+++ MAP.AspNetCore.StaticFiles/Actions/RegularFileName.cs
@@ -0,0 +1,76 @@
+﻿
+namespace MAP.AspNetCore.StaticFiles;
+
+public static class RegularFileName
+{
+    /// <summary>
+    /// This method creates a unique name for your file and short your file name to dont exception url
+    /// This method replace  "_" and "." from name to "-"
+    /// This method set file name with out Extension 
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public static string SetNameWithOutExtension(string name)
+    {
+        if (FileContentTypes.TryContentType(name))
+            return string.Join("", toEnDigits(string.Join(".", name.Split(".").Take(name.Split(".").Count() - 1)).Replace(".", "-").Replace("_", "-").Replace(" - ", "-").Replace(" ", "-")).ToCharArray().Take(50));
+        return string.Join("", toEnDigits(name.Replace(".", "-").Replace("_", "-").Replace(" - ", "-").Replace(" ", "-")).ToCharArray().Take(50));
+    }
+
+    /// <summary>
+    /// This method creates a unique name for your file and short your file name to dont exception url
+    /// This method replace  "_" and "." from name to "-"    
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public static string SetNameWithExtension(string name)
+    {
+        return string.Join("", toEnDigits(string.Join(".", name.Split(".").Take(name.Split(".").Count() - 1)).Replace(".", "-").Replace(" - ", "-").Replace(" ", "-")).ToCharArray().Take(50)) + "." + toEnDigits(name.Split(".").AsQueryable().Last());
+    }
+
+    /// <summary>
+    /// This method creates a unique name for your file and short your file name to dont exception url
+    /// This method replace  "_" and "." from name to "-"
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public static string SetNameWithExtension(string name, string extention)
+    {
+        return string.Join("", toEnDigits(name.Replace("_", "-").Replace(".", "-").Replace(" - ", "-").Replace(" ", "-")).ToCharArray().Take(50)) + "." + toEnDigits(extention.Split(".").AsQueryable().Last());
+    }
+
+    /// <summary>
+    /// This method remove uniqe code from file name and remove extension
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public static string RemoveGuidFromNameWithOutExtension(string name)
+    {
+        return toEnDigits(name.Replace("_" + name.Split("_")[1], string.Empty).Replace(" - ", "-").Replace(" ", "-"));
+    }
+
+    /// <summary>
+    /// This method remove uniqe code from file name 
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public static string RemoveGuidFromNameWithExtension(string name)
+    {
+        return toEnDigits(name.Replace("_" + name.Split("_")[1], string.Empty).Replace(" - ", "-").Replace(" ", "-") + "." + name.Split(".")[1]);
+    }
+
+    /// <summary>
+    /// This method change Persian/Arabic number to English number
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
+    public static string toEnDigits(string input)
+    {
+        string[] persian = new string[10] { "۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹" };
+
+        for (int j = 0; j < persian.Length; j++)
+            input = input.Replace(persian[j], j.ToString());
+
+        return input;
+    }
+}
---
+++ MAP.AspNetCore.StaticFiles/ViewComponents/UploadFormViewComponent.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace MAP.AspNetCore.StaticFiles.ViewComponents;
+
+[ViewComponent(Name = "UploadForm")]
+public class UploadFormViewComponent: ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
