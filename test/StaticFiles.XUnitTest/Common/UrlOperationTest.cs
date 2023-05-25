adeb0537c5186fde217ede43129bf49dc9355537
Mohammad Abbasi <133187637+Mohammad-Abbasi2559@users.noreply.github.com>
2023-05-25 03:37:53 -0700

    Add test and debug url

---
+++ test/StaticFiles.XUnitTest/Common/UrlOperationTest.cs
@@ -0,0 +1,37 @@
+﻿
+namespace StaticFiles.XUnitTest.Common;
+
+public class UrlOperationTest
+{
+    [Theory]
+    [InlineData("/admin/home/index", "/Admin/home/")]
+    [InlineData("/home/index", "/home/")]
+    public void CheckUrlTest(string path1, string path2)
+    {
+        Assert.True(UrlOperation.CheckUrl(path1, path2));
+    }
+
+    [Theory]
+    [InlineData("/admin/home/index", "/Admin/home/")]
+    [InlineData("/home/index", "/home/")]
+    public void CheckPathTest1(string path1, string path2)
+    {
+        Assert.True(UrlOperation.CheckPath(path1, path2));
+    }
+
+    [Theory]
+    [InlineData("admin/home/Index/", "/home/index")]
+    [InlineData("admin", "/home/index")]
+    public void CheckPathTest2(string path1, string path2)
+    {
+        Assert.Throws<ArgumentException>(() => UrlOperation.CheckPath(path1, path2));
+    }
+
+    [Theory]
+    [InlineData("admin/home", "  ")]
+    public void CheckPathTest3(string path1, string path2)
+    {
+        Assert.Throws<ArgumentNullException>(() => UrlOperation.CheckPath(path1, path2));
+    }
+
+}
