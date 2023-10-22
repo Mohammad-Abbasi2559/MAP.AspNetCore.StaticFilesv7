3d5ab3b09a2c5d9fd04232a30a618172fbef233f
Mohammad-Abbasi2559 <mohammad.abasy.79@gmail.com>
2023-10-22 04:18:57 -0700

    Add Test for CheckUrlWithOutParameter in UrlOperation

--- test/StaticFiles.XUnitTest/Common/UrlOperationTest.cs
+++ test/StaticFiles.XUnitTest/Common/UrlOperationTest.cs
@@ -4,11 +4,26 @@ namespace StaticFiles.XUnitTest.Common;
 public class UrlOperationTest
 {
     [Theory]
-    [InlineData("/admin/home/index", "/Admin/home/")]
-    [InlineData("/home/index", "/home/")]
-    public void CheckUrlWithOutParameterTest(string path1, string path2)
+    [InlineData("https://www.testdomain.com/admin/home/index", "https://www.testdomain.com/Admin/home/")]
+    [InlineData("https://www.testdomain.com/home/index", "https://www.testdomain.com/home/")]
+    public void CheckUrlWithOutParameterTest1(string path1, string path2)
     {
         Assert.True(UrlOperation.CheckUrlWithOutParameter(path1, path2));
+    }
+
+    [Theory]
+    [InlineData("https://www.testdomain.com/admin/home/Index/", "https://www.testdomain.com/home/index")]
+    [InlineData("https://www.testdomain.com/admin/", "https://www.testdomain.com/home/index")]
+    public void CheckUrlWithOutParameterTest2(string path1, string path2)
+    {
+        Assert.False(UrlOperation.CheckPathWithOutParameter(path1, path2));
+    }
+
+    [Theory]
+    [InlineData("https://www.testdomain.com/admin/home", "  ")]
+    public void CheckUrlWithOutParameterTest3(string path1, string path2)
+    {
+        Assert.Throws<ArgumentNullException>(() => UrlOperation.CheckPathWithOutParameter(path1, path2));
     }
 
     [Theory]
