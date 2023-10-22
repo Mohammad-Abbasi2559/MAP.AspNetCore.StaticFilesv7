fe063f611b49c3b3c8608f8297899389804a7bc4
Mohammad-Abbasi2559 <mohammad.abasy.79@gmail.com>
2023-10-22 09:16:30 -0700

    Optimize GetFileExtension method form FileExtension

--- src/MAP.AspNetCore.StaticFiles/Common/FileExtension.cs
+++ src/MAP.AspNetCore.StaticFiles/Common/FileExtension.cs
@@ -8,13 +8,13 @@ public static class FileExtension
     /// </summary>
     /// <param name="fileName"></param>
     /// <returns>return file extension</returns>
-    /// <exception cref="ArgumentNullException"></exception>
-    /// <exception cref="ArgumentException"></exception>
-    public static string? GetFileExtension(string fileName)
+    /// <exception cref="ArgumentNullException">Return exception if fileName be null</exception>
+    /// <exception cref="ArgumentException">Return exception if fileName doesnt have dot</exception>
+    public static string GetFileExtension(string fileName)
     {
-        if (string.IsNullOrWhiteSpace(fileName)) throw new ArgumentNullException("fileName is Rquired");
-        if (!fileName.Contains(".")) throw new ArgumentException("File dont have Extension");
+        if (string.IsNullOrWhiteSpace(fileName)) throw new ArgumentNullException(nameof(fileName));
+        if (!fileName.Contains('.')) throw new ArgumentException("File dont have Extension");
 
-        return fileName.Split('.').LastOrDefault();
+        return fileName[fileName.LastIndexOf('.')..];
     }
 }
