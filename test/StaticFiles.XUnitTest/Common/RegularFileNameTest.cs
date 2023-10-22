37207d3da4c82283cb82541e764883b5f7ebb652
Mohammad-Abbasi2559 <mohammad.abasy.79@gmail.com>
2023-10-22 12:16:08 -0700

    Add SetNameWithOutExtensionTest2 test to RegularFileNameTest class

--- test/StaticFiles.XUnitTest/Common/RegularFileNameTest.cs
+++ test/StaticFiles.XUnitTest/Common/RegularFileNameTest.cs
@@ -4,6 +4,8 @@ namespace StaticFiles.XUnitTest.Common;
 
 public class RegularFileNameTest
 {
+    private static readonly Regex AdditionalSpace = new("\\s+");
+
     private static Regex RegexWithOutExtension(string fileName) => new(fileName[..fileName.LastIndexOf('.')] + @"\S+");
 
     [Theory]
@@ -12,5 +14,7 @@ public class RegularFileNameTest
     [InlineData("test.mkv")]
     public void SetNameWithOutExtensionTest(string fileName) => Assert.Matches(RegexWithOutExtension(fileName), RegularFileName.SetNameWithOutExtension(fileName, out string? ext));
 
-    
+    [Theory]
+    [InlineData(" sfdff  _ fsf. svvs.txt")]
+    public void SetNameWithOutExtensionTest2(string fileName) => Assert.Matches(RegexWithOutExtension(AdditionalSpace.Replace(fileName, string.Empty)), RegularFileName.SetNameWithOutExtension(fileName, out string? ext));
 }
