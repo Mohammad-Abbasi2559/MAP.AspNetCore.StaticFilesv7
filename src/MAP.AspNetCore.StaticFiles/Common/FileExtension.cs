2c0b18f9b737d52d869b455a883dc48b812c6b52
Mohammad-Abbasi2559 <mohammad.abasy.79@gmail.com>
2023-10-22 09:23:55 -0700

    Add TryGetFileExtension method from FileExtension

--- src/MAP.AspNetCore.StaticFiles/Common/FileExtension.cs
+++ src/MAP.AspNetCore.StaticFiles/Common/FileExtension.cs
@@ -17,4 +17,24 @@ public static class FileExtension
 
         return fileName[fileName.LastIndexOf('.')..];
     }
+
+    /// <summary>
+    /// This method try get file extension from file name
+    /// </summary>
+    /// <param name="fileName"></param>
+    /// <param name="extension">Return file extension</param>
+    /// <returns>Return get file extension is work or not</returns>
+    /// <exception cref="ArgumentNullException"></exception>
+    /// <exception cref="ArgumentException"></exception>
+    public static bool TryGetFileExtension(string fileName, out string? extension)
+    {
+        if (string.IsNullOrWhiteSpace(fileName)) throw new ArgumentNullException(nameof(fileName));
+        if (!fileName.Contains('.')) throw new ArgumentException("File dont have Extension");
+
+        bool contentType = FileContentType.TryContentType(fileName);
+        
+        extension = contentType ? fileName[fileName.LastIndexOf('.')..] : null;
+
+        return contentType;
+    }
 }
