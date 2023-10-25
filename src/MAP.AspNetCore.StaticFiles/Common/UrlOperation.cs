f1057a3fae1b74512027c328171f38014b113cf8
Mohammad-Abbasi <mohammad.abasy.79@gmail.com>
2023-10-24 23:02:48 -0700

    fix: Solve problem of UrlOperation
    Method CheckUrlWithOutParameter and CheckPathWithOutParameter

--- src/MAP.AspNetCore.StaticFiles/Common/UrlOperation.cs
+++ src/MAP.AspNetCore.StaticFiles/Common/UrlOperation.cs
@@ -49,7 +49,7 @@ public static class UrlOperation
     /// <returns></returns>
     /// <exception cref="ArgumentNullException"></exception>
     /// <exception cref="ArgumentException"></exception>
-    public static bool CheckUrlWithOutParameter(string url1, string url2)
+    public static bool CheckUrlWithOutParameter(string url1, string url2, string? controllerName = null)
     {
         if (string.IsNullOrWhiteSpace(url1) || string.IsNullOrWhiteSpace(url2)) throw new ArgumentNullException("url1 or url2", "{0} is empty");
         if (!url1.Contains('/') || !url2.Contains('/')) throw new ArgumentException("url not correct");
@@ -72,17 +72,51 @@ public static class UrlOperation
             }
             else
             {
-                if (checkUrl1.Count == checkUrl2.Count + 1 && checkUrl2.Count > 1)
+                if (controllerName == null)
                 {
-                    for(int i = 0; i < checkUrl2.Count; i++) if (checkUrl1[i] != checkUrl2[i]) return false;
+                    if (checkUrl1.Count == checkUrl2.Count + 1 && checkUrl2.Count > 1)
+                    {
+                        for (int i = 0; i < checkUrl2.Count; i++) if (checkUrl1[i] != checkUrl2[i]) return false;
+                    }
+                    else if (checkUrl1.Count == checkUrl2.Count + 1 && urlSplit1.Length > 2) return checkUrl1[1] == urlSplit1.LastOrDefault();
+                    else if (checkUrl1.Count + 1 == checkUrl2.Count && checkUrl1.Count > 1)
+                    {
+                        for (int i = 0; i < checkUrl1.Count; i++) if (checkUrl1[i] != checkUrl2[i]) return false;
+                    }
+                    else if (checkUrl2.Count == checkUrl1.Count + 1 && urlSplit2.Length > 2) return checkUrl2[1] == urlSplit2.LastOrDefault();
+                    else return false;
                 }
-                else if (checkUrl1.Count == checkUrl2.Count + 1 && urlSplit1.Length > 2) return checkUrl1[1] == urlSplit1.LastOrDefault();
-                else if(checkUrl1.Count + 1 == checkUrl2.Count && checkUrl1.Count > 1)
+                else
                 {
-                    for(int i = 0; i < checkUrl1.Count; i++) if (checkUrl1[i] != checkUrl2[i]) return false;
+                    controllerName = controllerName.ToLower();
+                    if (checkUrl1.Count == checkUrl2.Count + 1 && checkUrl2.Count > 1)
+                    {
+                        int index1 = checkUrl1.IndexOf(controllerName);
+                        int index2 = checkUrl2.IndexOf(controllerName);
+
+                        string action1 = index1 > -1 && index1 + 1 < checkUrl1.Count ? checkUrl1[index1 + 1] : string.Empty;
+                        string action2 = index2 > -1 && index2 + 1 < checkUrl2.Count ? checkUrl2[index2 + 1] : string.Empty;
+
+                        for (int i = 0; i < checkUrl2.Count; i++) if (checkUrl1[i] != checkUrl2[i]) return false;
+
+                        return index1 > -1 && index2 > -1 && checkUrl1.Count - index1 == 3 && action1 == action2;
+                    }
+                    else if (checkUrl1.Count == checkUrl2.Count + 1 && urlSplit1.Length > 2) return checkUrl1[1] == urlSplit1.LastOrDefault();
+                    else if (checkUrl1.Count + 1 == checkUrl2.Count && checkUrl1.Count > 1)
+                    {
+                        int index1 = checkUrl1.IndexOf(controllerName);
+                        int index2 = checkUrl2.IndexOf(controllerName);
+
+                        string action1 = index1 > -1 && index1 + 1 < checkUrl1.Count ? checkUrl1[index1 + 1] : string.Empty;
+                        string action2 = index2 > -1 && index2 + 1 < checkUrl2.Count ? checkUrl2[index2 + 1] : string.Empty;
+
+                        for (int i = 0; i < checkUrl1.Count; i++) if (checkUrl1[i] != checkUrl2[i]) return false;
+
+                        return index1 > -1 && index2 > -1 && checkUrl2.Count - index2 == 3 && action1 == action2;
+                    }
+                    else if (checkUrl2.Count == checkUrl1.Count + 1 && urlSplit2.Length > 2) return checkUrl2[1] == urlSplit2.LastOrDefault();
+                    else return false;
                 }
-                else if (checkUrl2.Count == checkUrl1.Count + 1 && urlSplit2.Length > 2) return checkUrl2[1] == urlSplit2.LastOrDefault();
-                else return false;
             }
         }
 
@@ -97,7 +131,7 @@ public static class UrlOperation
     /// <returns></returns>
     /// <exception cref="ArgumentNullException"></exception>
     /// <exception cref="ArgumentException"></exception>
-    public static bool CheckPathWithOutParameter(string path1, string path2)
+    public static bool CheckPathWithOutParameter(string path1, string path2, string? controllerName = null)
     {
         if (string.IsNullOrWhiteSpace(path1) || string.IsNullOrWhiteSpace(path2)) throw new ArgumentNullException("path1 or path2", "url is empty");
         if (!path1.Contains('/') || !path2.Contains('/')) throw new ArgumentException("path not correct");
@@ -111,26 +145,61 @@ public static class UrlOperation
             List<string> checkPath1 = new();
             List<string> checkPath2 = new();
 
-            for (int i = 0; i < pathSplit1.Length; i++) if ((pathSplit1[i] != "home" || i + 1 < pathSplit1.Length && pathSplit1[ i + 1] != "index" && !string.IsNullOrEmpty(pathSplit1[i + 1])) && pathSplit1[i] != "index" && !string.IsNullOrEmpty(pathSplit1[i])) checkPath1.Add(pathSplit1[i]);
-            for (int i = 0; i < pathSplit2.Length; i++) if ((pathSplit2[i] != "home" || i + 1 < pathSplit2.Length && pathSplit2[ i + 1] != "index" && !string.IsNullOrEmpty(pathSplit2[i + 1])) && pathSplit2[i] != "index" && !string.IsNullOrEmpty(pathSplit2[i])) checkPath2.Add(pathSplit2[i]);
+            for (int i = 0; i < pathSplit1.Length; i++) if ((pathSplit1[i] != "home" || i + 1 < pathSplit1.Length && pathSplit1[i + 1] != "index" && !string.IsNullOrEmpty(pathSplit1[i + 1])) && pathSplit1[i] != "index" && !string.IsNullOrEmpty(pathSplit1[i])) checkPath1.Add(pathSplit1[i]);
+            for (int i = 0; i < pathSplit2.Length; i++) if ((pathSplit2[i] != "home" || i + 1 < pathSplit2.Length && pathSplit2[i + 1] != "index" && !string.IsNullOrEmpty(pathSplit2[i + 1])) && pathSplit2[i] != "index" && !string.IsNullOrEmpty(pathSplit2[i])) checkPath2.Add(pathSplit2[i]);
 
             if (checkPath1.Count == checkPath2.Count)
             {
-                for(int i = 0; i < checkPath1.Count; i++) if (checkPath1[i] != checkPath2[i]) return false;
+                for (int i = 0; i < checkPath1.Count; i++) if (checkPath1[i] != checkPath2[i]) return false;
             }
             else
             {
-                if (checkPath1.Count == checkPath2.Count + 1 && checkPath2.Count > 0)
+                if (controllerName == null)
                 {
-                    for(int i = 0; i < checkPath2.Count; i++) if (checkPath1[i] != checkPath2[i]) return false;
+                    if (checkPath1.Count == checkPath2.Count + 1 && checkPath2.Count > 0)
+                    {
+                        for (int i = 0; i < checkPath2.Count; i++) if (checkPath1[i] != checkPath2[i]) return false;
+                    }
+                    else if (checkPath1.Count == checkPath2.Count + 1 && pathSplit1.Length > 2) return checkPath1[0] == pathSplit1.LastOrDefault();
+                    else if (checkPath1.Count + 1 == checkPath2.Count && checkPath1.Count > 0)
+                    {
+                        for (int i = 0; i < checkPath1.Count; i++) if (checkPath1[i] != checkPath2[i]) return false;
+                    }
+                    else if (checkPath2.Count == checkPath1.Count + 1 && pathSplit2.Length > 2) return checkPath2[0] == pathSplit2.LastOrDefault();
+                    else return false;
                 }
-                else if (checkPath1.Count == checkPath2.Count + 1 && pathSplit1.Length > 1) return checkPath1[0] == pathSplit1.LastOrDefault();
-                else if(checkPath1.Count + 1 == checkPath2.Count && checkPath1.Count > 0)
+                else
                 {
-                    for(int i = 0; i < checkPath1.Count; i++) if (checkPath1[i] != checkPath2[i]) return false;
+                    controllerName = controllerName.ToLower();
+                    if (checkPath1.Count == checkPath2.Count + 1 && checkPath2.Count > 0)
+                    {
+                        int index1 = checkPath1.IndexOf(controllerName);
+                        int index2 = checkPath2.IndexOf(controllerName);
+
+                        string action1 = index1 > -1 && index1 + 1 < checkPath1.Count ? checkPath1[index1 + 1] : string.Empty;
+                        string action2 = index2 > -1 && index2 + 1 < checkPath2.Count ? checkPath2[index2 + 1] : string.Empty;
+
+
+                        for (int i = 0; i < checkPath2.Count; i++) if (checkPath1[i] != checkPath2[i]) return false;
+
+                        return index1 > -1 && index2 > -1 && checkPath1.Count - index1 == 3 && action1 == action2;
+                    }
+                    else if (checkPath1.Count == checkPath2.Count + 1 && pathSplit1.Length > 2) return checkPath1[0] == pathSplit1.LastOrDefault();
+                    else if (checkPath1.Count + 1 == checkPath2.Count && checkPath1.Count > 0)
+                    {
+                        int index1 = checkPath1.IndexOf(controllerName);
+                        int index2 = checkPath2.IndexOf(controllerName);
+
+                        string action1 = index1 > -1 && index1 + 1 < checkPath1.Count ? checkPath1[index1 + 1] : string.Empty;
+                        string action2 = index2 > -1 && index2 + 1 < checkPath2.Count ? checkPath2[index2 + 1] : string.Empty;
+
+                        for (int i = 0; i < checkPath1.Count; i++) if (checkPath1[i] != checkPath2[i]) return false;
+
+                        return index1 > -1 && index2 > -1 && checkPath2.Count - index2 == 3 && action1 == action2;
+                    }
+                    else if (checkPath2.Count == checkPath1.Count + 1 && pathSplit2.Length > 2) return checkPath2[0] == pathSplit2.LastOrDefault();
+                    else return false;
                 }
-                else if (checkPath2.Count == checkPath1.Count + 1 && pathSplit2.Length > 1) return checkPath2[0] == pathSplit2.LastOrDefault();
-                else return false;
             }
         }
 
