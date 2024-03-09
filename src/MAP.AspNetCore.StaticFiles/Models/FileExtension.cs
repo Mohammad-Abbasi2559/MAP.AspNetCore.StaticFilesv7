b7132842865c7996c52a38cf700ae6e71d2822f3
Mohammad-Abbasi <mohammad.abasy.79@gmail.com>
2024-03-09 15:49:55 +0330

    Add property to FileExtension.cs class
    Signed-off-by: Mohammad-Abbasi <mohammad.abasy.79@gmail.com>

--- src/MAP.AspNetCore.StaticFiles/Models/FileExtension.cs
+++ src/MAP.AspNetCore.StaticFiles/Models/FileExtension.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MAP.AspNetCore.StaticFiles.Models;
 
 public class FileExtension
@@ -5,4 +7,12 @@ public class FileExtension
     public string Extension { get; set; } = string.Empty;
 
     public string MimeType { get; set; } = string.Empty;
+
+
+
+
+    [Required]
+    public List<FileHeader>? FirstChunkBytes { get; set; }
+
+    public List<FileHeader>? LastChunkBytes { get; set; }
 }
