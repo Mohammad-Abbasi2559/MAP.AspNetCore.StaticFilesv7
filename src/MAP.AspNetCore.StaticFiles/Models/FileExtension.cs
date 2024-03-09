1d936b131f68bcec100e54b26bfce6b4d6bf8a8e
Mohammad-Abbasi <mohammad.abasy.79@gmail.com>
2024-03-09 15:33:25 +0330

    Add FileExtension class
    Signed-off-by: Mohammad-Abbasi <mohammad.abasy.79@gmail.com>

---
+++ src/MAP.AspNetCore.StaticFiles/Models/FileExtension.cs
@@ -0,0 +1,8 @@
+namespace MAP.AspNetCore.StaticFiles.Models;
+
+public class FileExtension
+{
+    public string Extension { get; set; } = string.Empty;
+
+    public string MimeType { get; set; } = string.Empty;
+}
